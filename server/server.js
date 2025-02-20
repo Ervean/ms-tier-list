f5460ca02b2d849eb6fac49699ce52c5c1de0da6
Ethan Vukhoi Nguyen <ethanswer@gmail.com>
2025-02-20 11:47:17 -0800

    Added example put for modifying description of a job

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { query } from "express";
 import cors from 'cors';
 import pool from "./db.js";
 
@@ -26,7 +26,7 @@ app.get("/jobs/:id", async(req, res) => {
     try {
         console.log(req.params);
         const { id } = req.params;
-        const job = await pool.query(`SELECT * FROM jobs WHERE id = ${id}`);
+        const job = await pool.query("SELECT * FROM jobs WHERE id = $1",[id]);
         if(job.rowCount >= 1) {
             res.json(job.rows[0]);
         } else {
@@ -36,6 +36,20 @@ app.get("/jobs/:id", async(req, res) => {
         console.error(err.message);
     }
 });
+
+//update description
+app.put("/jobs/description/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        console.log("trying to update description for " + id);
+        const { description } = req.body;
+        const updateJobDescription = await pool.query("UPDATE jobs SET description = $1 WHERE id = $2", [description, id]);
+        res.json(`Job id: ${id} description was updated to ${description}`);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
 
 app.get("/api/home", (req, res) => {
     res.json({ message:  "Hello World!" });
